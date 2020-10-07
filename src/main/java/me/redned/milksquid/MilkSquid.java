7b7a6f1a90894ee4b2a635500c41d5d12547b530
RednedEpic <redned235@gmail.com>
2020-10-06 21:22:49 -0500

    Improve some stuff

--- src/main/java/me/redned/milksquid/MilkSquid.java
+++ src/main/java/me/redned/milksquid/MilkSquid.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Squid;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,9 +23,17 @@ public final class MilkSquid extends JavaPlugin implements Listener {
         if (!(event.getRightClicked() instanceof Squid)) {
             return;
         }
+        if (event.getHand() != EquipmentSlot.HAND) {
+            return;
+        }
         Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.BUCKET) {
-            player.getInventory().setItemInMainHand(new ItemStack(Material.MILK_BUCKET));
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (item == null) {
+            return;
+        }
+        if (item.getType() == Material.BUCKET) {
+            item.setAmount(item.getAmount() - 1);
+            player.getInventory().addItem(new ItemStack(Material.MILK_BUCKET));
             player.playSound(player.getLocation(), Sound.ENTITY_COW_MILK, 1f, 1f);
         }
     }
